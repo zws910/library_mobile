796a1d72155cdf79d63efade5d6a892788ca4835
Vincent Zheng <vincentzheng0910@gmail.com>
2020-07-07 10:22:23 +0800

    remove all react files

--- library-app/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import AppRouter from "./router/AppRouter";
-import "./statics/css/common.less";
-import "./statics/iconfont/iconfont.css"
-
-ReactDOM.render(<AppRouter></AppRouter>, document.getElementById("root"));
